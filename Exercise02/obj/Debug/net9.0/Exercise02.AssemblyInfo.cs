4019f70c926d31d2c90fa62af22c171f5d9ad69a
amyportillo <amyportllo@gmail.com>
2025-06-12 16:46:24 -0400

    12.2

--- Exercise02/obj/Debug/net9.0/Exercise02.AssemblyInfo.cs
+++ Exercise02/obj/Debug/net9.0/Exercise02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7fd9713fb23f7cb88ba669b99391d0e1b3a0aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
